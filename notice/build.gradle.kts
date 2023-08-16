885efbf8503a7e4c20abd5e0226d3c98e0889329
yujamint <woawoa0513@gmail.com>
2023-08-17 08:35:57 +0900

    chore: 프로젝트 설정

---
+++ notice/build.gradle.kts
@@ -0,0 +1,60 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "3.1.2"
+	id("io.spring.dependency-management") version "1.1.2"
+	kotlin("jvm") version "1.8.22"
+	kotlin("plugin.spring") version "1.8.22"
+	kotlin("plugin.jpa") version "1.8.22"
+
+	// ktlint
+	id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	runtimeOnly("com.h2database:h2")
+	runtimeOnly("com.mysql:mysql-connector-j")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+	// RestAssured
+	testImplementation("io.rest-assured:rest-assured:5.3.0")
+
+	// slack api
+	implementation("com.slack.api:slack-api-client:1.30.0")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs += "-Xjsr305=strict"
+		jvmTarget = "17"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+buildscript {
+	repositories {
+		maven(url = "https://plugins.gradle.org/m2/")
+	}
+	dependencies {
+		classpath("org.jlleitschuh.gradle:ktlint-gradle:10.2.1")
+	}
+}
---
+++ notice/src/main/kotlin/com/example/notice/NoticeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.notice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NoticeApplication
+
+fun main(args: Array<String>) {
+	runApplication<NoticeApplication>(*args)
+}
